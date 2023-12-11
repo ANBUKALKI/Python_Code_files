66790a4c5867ad6052b67e3f716ca16cc9a65cf2
ANBARASAN.P <110836351+ANBUKALKI@users.noreply.github.com>
2023-12-11 13:16:38 +0530

    Add files via upload

---
+++ Prettytable coding.py
@@ -0,0 +1,9 @@
+from prettytable import PrettyTable
+mytable = PrettyTable(["Student name","class","section","percentage"])
+mytable.add_row(["Anbu","12","b","78%"])
+mytable.add_row(["Anbu","12","b","78%"])
+mytable.add_row(["Anbu","12","b","78%"])
+mytable.add_row(["Anbu","12","b","78%"])
+mytable.add_row(["Anbu","12","b","78%"])
+mytable.add_row(["Anbu","12","b","78%"])
+print(mytable)
